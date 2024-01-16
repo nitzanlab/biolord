b33d9a7f36ecff05b8ce03127a5b3afcdc0d6a6a
Zoe <zoe.piran@mail.huji.ac.il>
2024-01-16 14:42:41 +0200

    refs

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -21,6 +21,5 @@ def test_biolord():
         adata=adata,
         n_latent=n_latent,
     )
-    model.train(10, check_val_every_n_epoch=1, train_size=0.5, enable_checkpointing=False)
 
     print(model)
